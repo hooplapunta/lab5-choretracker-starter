4d2809efbbb2c9022a6087a1a0d350580fe0440b
Chew Rong Kang <chewrongkang@gmail.com>
2021-02-08 01:13:44 +0800

    fix models for test

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,9 @@
 class Child < ApplicationRecord
     
+    has_many :chores
+    has_many :tasks, through: :chores
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
 end
