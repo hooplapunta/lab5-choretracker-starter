02b4e969067318fd9119986f8d1761737549d366
Chew Rong Kang <chewrongkang@gmail.com>
2021-02-08 02:26:50 +0800

    task tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+
+    has_many :chores
+    has_many :children, through: :chores
+
+    validates :name, presence: true
+    validates :points, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    scope :alphabetical, -> { order(name: :asc) }
+    scope :active, -> { where(active: true).order(name: :asc) }
+
 end
